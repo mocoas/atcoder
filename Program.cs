a497e7315438a42251b0aadde852dbc1047870de
mocoas <moco.as@gmail.com>
2019-07-28 17:30:06 +0900

    Atcoder Begining sellection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace atcoder
 {
@@ -6,7 +7,74 @@ namespace atcoder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PracticeC.test();
         }
     }
+
+    class PracticeC
+    {
+        public static void test()
+        {
+            char[] s = Input.getString().ToCharArray();
+            int result = 0;
+            
+            for (int i = 0; i<s.Length; i++)
+            {
+                if(s[i]=='1')
+                {
+                    result ++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+
+    class Input
+    {
+        public static string getString()
+        {
+            string s = Console.ReadLine();
+            return s;
+        }
+        public static int[] getIntArray()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int[] num = s.Select(x=>int.Parse(x)).ToArray();
+            return num;
+        }
+    }
+
+    class PracticeB
+    {
+        public static void test()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int[] num = s.Select(x=>int.Parse(x)).ToArray();
+
+            if(num[0]%2==0 || num[1]%2==0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+    class PracticeA
+    {
+        public static void test()
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] b = Console.ReadLine().Split(' ');
+            int[] bc = b.Select(x=>int.Parse(x)).ToArray();
+            string s = Console.ReadLine();
+            
+            
+            int abc = a + bc[0]+bc[1];
+            var result = abc + " " + s;   
+            Console.WriteLine(result);
+        }
+    }
+    
 }
