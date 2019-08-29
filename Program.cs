b55baf414bb01e58ccc0dd99cedf8e924af9dd75
momoko.asano <Momoko.Asano@i-vinci.co.jp>
2019-08-29 15:51:15 +0900

    solve 128B

--- Program.cs
+++ Program.cs
@@ -1,112 +1,82 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace atcoder
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            var a = Console.ReadLine().Split(' ');
-            int[] w = a.Select(x=>int.Parse(x)).ToArray();
-        
-            int sum = w.Sum();
-            int result =sum;
+namespace atcoder {
+    class Program {
+        static void Main (string[] args)
+         {
+            int n = int.Parse (Console.ReadLine ());
+            var dic = new Dictionary<Tuple<int,string>,int>();
+            string[] s = Input.getStringArray (n);
 
-            for(int i = 0; i<n-1; i++)
-            {
-                int s1 = 0;
-                int s2 = 0;
+            for (int i = 0; i < n; i++) {
 
-                for(int j = 0; j<=i; j++)
-                {
-                    s1= s1 + w[j];
-                }
-                s2 = sum-s1;
-                
-                if(result > Math.Abs(s2-s1))
-                {
-                    result = Math.Abs(s2-s1);
-                }
+                var str = s[i].Split (' ');
+                dic.Add(Tuple.Create(i+1,str[0]), int.Parse(str[1]));
 
             }
 
-            Console.WriteLine(result);
+            foreach(var item in dic.OrderBy(x => x.Key.Item2).ThenByDescending(x => x.Value))
+            {
+                Console.WriteLine(item.Key.Item1);
+            }
+
         }
     }
 
-    class Output
-    {
-        public static void writeIntArraySprit(int[] array)
-        {
-            for(int i=0; i<array.Length; i++)
-            {
-                if(i == array.Length-1)
-                {
-                    Console.Write(i);
+    class Output {
+        public static void writeIntArraySprit (int[] array) {
+            for (int i = 0; i < array.Length; i++) {
+                if (i == array.Length - 1) {
+                    Console.Write (i);
+                } else {
+                    Console.Write (i + " ");
                 }
-                else
-                {
-                    Console.Write(i+" ");    
-                }
-                
+
             }
         }
     }
 
-    class Input
-    {
-        public static int[] getIntArray()
-        {
-            string[] s = Console.ReadLine().Split(' ');
-            int[] num = s.Select(x=>int.Parse(x)).ToArray();
+    class Input {
+        public static int[] getIntArray () {
+            string[] s = Console.ReadLine ().Split (' ');
+            int[] num = s.Select (x => int.Parse (x)).ToArray ();
             return num;
         }
-        public static long[] getLongArray()
-        {
-            string[] s = Console.ReadLine().Split(' ');
-            long[] num = s.Select(x=>long.Parse(x)).ToArray();
+        public static long[] getLongArray () {
+            string[] s = Console.ReadLine ().Split (' ');
+            long[] num = s.Select (x => long.Parse (x)).ToArray ();
             return num;
         }
-        public static string[] getStringArray(int n)
-        {
+        public static string[] getStringArray (int n) {
             var s = new string[n];
-            for(int i=0; i<n; i++)
-            {
-                s[i] = Console.ReadLine();
+            for (int i = 0; i < n; i++) {
+                s[i] = Console.ReadLine ();
             }
             return s;
         }
     }
 
-    class Calc
-    {
+    class Calc {
         //Greatest common divisor
-        public static int Gcd(int a, int b)
-        { 
-            if(a<b)
-            {
-                return Gcd(b,a);
-            }
-            else
-            {
-                while(b != 0)
-                {
-                    int tmp = a%b;
+        public static int Gcd (int a, int b) {
+            if (a < b) {
+                return Gcd (b, a);
+            } else {
+                while (b != 0) {
+                    int tmp = a % b;
                     a = b;
                     b = tmp;
                 }
-                return a;                
+                return a;
             }
 
         }
 
         //Least common multiple
-        public static long Lcm(int a, int b)
-        {
-            return (long)(a*b/Gcd(a,b));
+        public static long Lcm (int a, int b) {
+            return (long) (a * b / Gcd (a, b));
         }
     }
 }
